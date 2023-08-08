4ec779d63b97b008cdc7fdf0763291b3bd08e0c5
andrepazvi <andrea.paz.villarroel@gmail.com>
2023-08-08 17:25:32 -0400

    resuelto conflicto 2

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -4,20 +4,10 @@ import "./ItemDetail.css";
 import ItemCount from "../ItemCount/ItemCount";
 import { CartContext } from '../../context/CartContext';
 
-<<<<<<< HEAD
 const ItemDetail = ({ product }) => {
 
   const [disabledButton, setDisabledButton] = useState(true);
   const { addItem } = useContext(CartContext);
-=======
-import { CartContext } from '../../context/CartContext';
-
-const ItemDetail = ({ product }) => {
-
-  const [disabledButton, setDisabledButton] = useState(true)
-
-  const { addItem } = useContext(CartContext)
->>>>>>> 4cda5213ed9122dd84593e520d6d6e0b09c48096
 
   const handleAddToCart = (quantity) => {
     addItem({ 
@@ -25,11 +15,7 @@ const ItemDetail = ({ product }) => {
       name: product.name,
       price: product.price
     }, quantity);
-<<<<<<< HEAD
     setDisabledButton(false);
-=======
-    setDisabledButton(true);
->>>>>>> 4cda5213ed9122dd84593e520d6d6e0b09c48096
   };
 
   return (
@@ -53,7 +39,7 @@ const ItemDetail = ({ product }) => {
         />
       ) : (
         <footer>
-          <Link to="/cart" className="Option"> {}
+          <Link to="/cart" className="Option">
             Terminar Compra
           </Link>
         </footer>
